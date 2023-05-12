b6a229c2a33df41118b5ce9a61cbc91bfbec9b8a
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-05-12 11:39:57 -0500

    barberia

--- src/Agenda/Agenda.jsx
+++ src/Agenda/Agenda.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
 export function Agenda() {
+
+    const[nombre,setNombre]=useState(null)
+    const[correo, setCorreo]=useState(null)
+    const[telefono,setTelefono]=useState(null)
+    const[fecha,setFecha]=useState(null)
+    const[hora, setHora]=useState(null)
 
     return (
 
@@ -10,13 +17,28 @@ export function Agenda() {
                     <div className="col-12 col-md-6">
                         <div className="input-group mb-3">
                             <span className="input-group-text" id="basic-addon1">@</span>
-                            <input type="text" className="form-control" placeholder="Nombre Cliente" />
+                            <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="Nombre Cliente" 
+                            id="nombre"
+                            onChange={(evento)=>{
+                                setNombre(evento.target.value)
+                            }}
+                            />
                         </div>
                     </div>
                     <div className="col-12 col-md-6">
                         <div className="input-group mb-3">
                             <span className="input-group-text" id="basic-addon1">@</span>
-                            <input type="text" className="form-control" placeholder="Correo cliente" />
+                            <input
+                             type="text" 
+                             className="form-control"
+                              placeholder="Correo cliente"
+                               id="email"
+                               onChange={(evento)=>{
+                                setCorreo(evento.target.value)
+                            }} />
                         </div>
                     </div>
                 </div>
@@ -25,7 +47,13 @@ export function Agenda() {
                     <div className="col-12">
                         <div className="input-group mb-3">
                             <span className="input-group-text" id="basic-addon1">@</span>
-                            <input type="text" className="form-control" placeholder="Telefono Cliente" />
+                            <input type="text" 
+                            className="form-control" 
+                            placeholder="Telefono Cliente"
+                            id="telefono"
+                            onChange={(evento)=>{
+                                setTelefono(evento.target.value)
+                            }} />
                         </div>
                     </div>
                 </div>
@@ -34,13 +62,27 @@ export function Agenda() {
                     <div className="col-12 col-md-6">
                         <div className="input-group mb-3">
                             <span className="input-group-text" id="basic-addon1">@</span>
-                            <input type="date" className="form-control" placeholder="Telefono Cliente" />
+                            <input 
+                            type="date" 
+                            className="form-control" 
+                            placeholder="Date" 
+                            id="fecha"
+                            onChange={(evento)=>{
+                                setFecha(evento.target.value)
+                            }}/>
                         </div>
                     </div>
                     <div className="col-12 col-md-6">
                         <div className="input-group mb-3">
-                            <span className="input-group-text" id="basic-addon1">@</span>
-                            <select className="form-select"  defaultValue={'DEFAULT'}>
+                            <span className="input-group-text" 
+                            id="basic-addon1">@</span>
+                            <select 
+                            id="hora"
+                            onChange={(evento)=>{
+                                setHora(evento.target.value)
+                            }}
+                            className="form-select"
+                              defaultValue={'DEFAULT'}>
                                 <option value="DEFAULT">Hora:</option>
                                 <option value="1">6:30 am</option>
                                 <option value="2">7:00 am</option>
@@ -50,7 +92,7 @@ export function Agenda() {
                     </div>
                 </div>
 
-
+                <button type="submit" className="btn btn-primary">Reserva Ya</button>
 
             </form>
 
---
+++ src/shared/Carga/Carga.jsx
@@ -0,0 +1,20 @@
+import './Carga.css'
+export function Carga(){
+    return(
+        <>
+        <div className="container fondo">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-5 text-center text-white">
+                    <br /><br /><br />
+                     <h2>Bienvenido</h2>
+                     <img src="../../src/assets/img/shop.gif" alt="" className="mx-auto d-block img-fluid mx-auto d-block"  />
+                     <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex ullam rerum unde! Sint eum explicabo veritatis qui iure repellat, quas cumque consectetur tempora autem dignissimos facilis, quidem exercitationem non omnis?
+                     </p>
+                </div>
+            </div>
+        </div>
+        
+        </>
+    )
+}
